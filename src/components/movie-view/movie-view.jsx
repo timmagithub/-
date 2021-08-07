9253d11886b5e3e3b01bb4e670e66c5d3a86bae1
timmagithub <80230635+timmagithub@users.noreply.github.com>
2021-08-07 01:36:08 -0400

    Cleaned code.

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -2,25 +2,24 @@ import React from 'react';
 
 export class MovieView extends React.Component {
 
-    render() {
-      const { movie, onBackClick } = this.props;
+  render() {
+    const { movie, onBackClick } = this.props;
   
-      return (
-        <div className="movie-view">
-          <div className="movie-poster">
-            <img src={movie.image} />
-          </div>
-          <div className="movie-title">
-            <span className="label">Title: </span>
-            <span className="value">{movie.title}</span>
-          </div>
-          <div className="movie-description">
-            <span className="label">Description: </span>
-            <span className="value">{movie.description}</span>
-          </div>
-          <button onClick={() => { onBackClick(null); }}>Back</button>
-  
-        </div>
+  return (
+    <div className="movie-view">
+      <div className="movie-poster">
+        <img src={movie.image} />
+      </div>
+      <div className="movie-title">
+        <span className="label">Title: </span>
+        <span className="value">{movie.title}</span>
+      </div>
+      <div className="movie-description">
+        <span className="label">Description: </span>
+        <span className="value">{movie.description}</span>
+      </div>
+        <button onClick={() => { onBackClick(null); }}>Back</button>
+    </div>
       );
     }
   }
