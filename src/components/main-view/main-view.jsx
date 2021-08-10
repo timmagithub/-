e14377a17ff04dbc26863ca06d362779eebdb636
timmagithub <80230635+timmagithub@users.noreply.github.com>
2021-08-09 23:22:12 -0400

    Update main-view.jsx
    Added registration integration

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 
 import { LoginView } from '../login-view/login-view';
+import { RegistrationView } from '../registration-view/registration-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -14,6 +15,7 @@ export class MainView extends React.Component {
             movies: [],
             selectedMovie: null,
             user: null
+            register: null
         };
     }
 
@@ -35,6 +37,12 @@ export class MainView extends React.Component {
         });
     }
 
+    onRegistration(register) {
+        this.setState({
+            register
+        });
+    }
+
     onLoggedIn(user) {
         this.setState({
             user
@@ -44,6 +52,11 @@ export class MainView extends React.Component {
     render() {
         const { movies, selectedMovie } = this.state;
     
+        const { movies, selectedMovie, register, user } = this.state;
+
+        /* loads registration screen */
+        if (!register) return <RegistrationView onRegistration={(register) => this.onRegistration(register)} />; 
+
         /* loads login view if no user logged in */
         if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
 
