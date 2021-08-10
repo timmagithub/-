53e1bcb7c19d1e85eb486d048beafcf030a2f06a
timmagithub <80230635+timmagithub@users.noreply.github.com>
2021-08-09 23:21:08 -0400

    Update login-view.jsx
    created login view

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,28 +1,38 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
 export function LoginView(props) {
-    const [ username, setUsername ] = useState('');
+    const [ userName, setUsername ] = useState('');
     const [ password, setPassword ] = useState('');
     
+    
     /* send request to server to authenticate then call props.onLoggedIn(username) */
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(username, password);
-        props.onLoggedIn(username);
-    };
+        console.log(userName, password);
+        props.onLoggedIn(userName);
+      };
 
     /* form inserted to login */
     return (
         <form>
             <label>
                 Username:
-                <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+                <input type="text" value={userName} onChange={(e) => setUsername(e.target.value)} />
             </label>
             <label>
                 Password:
-                <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
             </label>
-            <button type="submit" onClick={handleSubmit}>Submit</button>
+            <button type="submit" onClick={(e) => handleSubmit}>Submit</button>
         </form>
     );
 }
+
+LoginView.propTypes = {
+    user: PropTypes.shape({
+        userName: PropTypes.string.isRequired,
+        password: PropTypes.string.isRequired
+    }),
+    onLoggedIn: PropTypes.func.isRequired
+};
