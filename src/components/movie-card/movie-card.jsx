2c96b2579bec80a9f9a466abd4052d2bc9357474
timmagithub <80230635+timmagithub@users.noreply.github.com>
2021-08-18 22:42:24 -0400

    Update movie-card.jsx
    incorporate bootstrap and styling.

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,8 +1,43 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+
+import './movie-card.scss';
 
 export class MovieCard extends React.Component {
     render() {
         const {movie, onMovieClick} = this.props;
-        return <div className="movie-card" onClick={() => {onMovieClick(movie); }}>{movie.title}</div>;
+        return ( 
+            <Container className="card-container">
+                <Row>
+                    <Col>
+                        <Card className="movie-card">
+                            <Card.Img variant="top" src={movie.image} />
+
+                            <Card.Body className="card-body">
+                                <Card.Title>{movie.title}</Card.Title>
+                                <Card.Text>{movie.year}</Card.Text>
+                                <Button onClick={() => onMovieClick(movie)} variant="primary">
+                                    More Details
+                                </Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+            );
     }
 }
+
+MovieCard.PropTypes = {
+    movie: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired,
+        year: PropTypes.string.isRequired
+    }),
+    onClick: PropTypes.func.isRequired
+};
