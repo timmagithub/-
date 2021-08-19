f8a2f312922256a763fefa5c63680f8014bd1553
timmagithub <80230635+timmagithub@users.noreply.github.com>
2021-08-18 22:41:11 -0400

    Update main-view.jsx
    incorporate bootstrap into the main-view.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import axios from 'axios';
+import Container  from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import './main-view.scss';
 
 import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import { RegistrationView } from '../registration-view/registration-view';
 
 
 export class MainView extends React.Component {
@@ -13,7 +19,8 @@ export class MainView extends React.Component {
         this.state = {
             movies: [],
             selectedMovie: null,
-            user: null
+            user: null,
+            register: null
         };
     }
 
@@ -35,6 +42,12 @@ export class MainView extends React.Component {
         });
     }
 
+    onRegistration(register) {
+        this.setState({
+            register
+        });
+    }
+
     onLoggedIn(user) {
         this.setState({
             user
@@ -42,7 +55,10 @@ export class MainView extends React.Component {
     }
 
     render() {
-        const { movies, selectedMovie } = this.state;
+        const { movies, selectedMovie, user, register } = this.state;
+
+        /* loads registration screen */
+        if (!register) return <RegistrationView onRegistration={(register) => this.onRegistration(register)} />;
     
         /* loads login view if no user logged in */
         if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
@@ -50,16 +66,25 @@ export class MainView extends React.Component {
         if (movies.length === 0) return <div className="main-view" />;
     
         return (
-          <div className="main-view">
-            {selectedMovie
-              ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => 
-                    { this.setSelectedMovie(newSelectedMovie); }}/>
-              : movies.map(movie => (
-                <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => 
-                    { this.setSelectedMovie(newSelectedMovie) }}/>
-              ))
-            }
-          </div>
+            <Container>
+            <Row className="main-view justify-content-md-center">
+                {selectedMovie
+                    ? (
+                        <Col md={8}>
+                            <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => 
+                                { this.setSelectedMovie(newSelectedMovie); }}/>
+                        </Col>
+                    
+                    )
+                    : movies.map(movie => (
+                        <Col md="3" className="movie-card-callback">
+                            <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => 
+                                { this.setSelectedMovie(newSelectedMovie) }}/>
+                        </Col>
+                    ))
+                }
+            </Row>
+          </Container>
         );
       }
     
