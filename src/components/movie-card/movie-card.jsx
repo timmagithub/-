98ec10981f3ab7e7ea3151d2442b448032a0f7b7
timmagithub <80230635+timmagithub@users.noreply.github.com>
2021-10-09 12:53:40 -0400

    updated to use link router

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,32 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
+import { Link } from 'react-router-dom';
 
 import './movie-card.scss';
 
 export class MovieCard extends React.Component {
     render() {
-        const {movie, onMovieClick} = this.props;
+
+        const { movie } = this.props;
+
         return ( 
             <Container className="card-container">
-                <Row>
-                    <Col>
-                        <Card className="movie-card">
-                            <Card.Img variant="top" src={movie.image} />
+                <Card className="movie-card">
+                    <Card.Img variant="top" src={movie.image} />
 
-                            <Card.Body className="card-body">
-                                <Card.Title>{movie.title}</Card.Title>
-                                <Card.Text>{movie.year}</Card.Text>
-                                <Button onClick={() => onMovieClick(movie)} variant="primary">
-                                    More Details
-                                </Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
+                        <Card.Body className="card-body">
+                            <Card.Title>{movie.title}</Card.Title>
+                            <Card.Text>{movie.year}</Card.Text>
+                            <Link to={'/movies/${movie._id}'} >
+                                <Button variant="link">Open</Button>
+                            </Link>
+                        </Card.Body>
+                    </Card>
             </Container>
             );
     }
