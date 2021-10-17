8031b004db847388c297740935476f1622fa1a75
timmagithub <80230635+timmagithub@users.noreply.github.com>
2021-10-17 13:52:17 -0400

    button to add movie to list & fixd react errors

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
@@ -9,20 +10,37 @@ import './movie-card.scss';
 
 export class MovieCard extends React.Component {
     render() {
+        const handleSubmit = (e) => {
+            const token = localStorage.getItem('token');
+            const user = localStorage.getItem('user');
+            axios.post(`https://quikflix.herokuapp.com/users/${user}/myMovies/` + 
+                this.props.movie._id, {}, {headers: { Authorization: `Bearer ${token}` }, })
+                .then(response => {
+                    const data = response.data;
+                    console.log(data);
+                    alert('Movie was added to your favorites. (If this was done in error, go to profile page to remove it.');
+                })
+                .catch(e=> {
+                    console.log('error adding movie to your favorites')
+                });
+          };
 
         const { movie } = this.props;
 
         return ( 
             <Container className="card-container">
                 <Card className="movie-card">
-                    <Card.Img variant="top" src={movie.image} />
+                    <Card.Img crossOrigin="anonymous" variant="top" src={movie.image} />
 
                         <Card.Body className="card-body">
                             <Card.Title>{movie.title}</Card.Title>
                             <Card.Text>{movie.year}</Card.Text>
-                            <Link to={'/movies/${movie._id}'} >
+                            <Link to={`/movies/${movie.title}`} >
                                 <Button variant="link">Open</Button>
                             </Link>
+                            <Button variant="primary" type="submit" onClick={handleSubmit}>
+                                Add to Favs
+                            </Button>
                         </Card.Body>
                     </Card>
             </Container>
@@ -30,11 +48,11 @@ export class MovieCard extends React.Component {
     }
 }
 
-MovieCard.PropTypes = {
+MovieCard.propTypes = {
     movie: PropTypes.shape({
         title: PropTypes.string.isRequired,
         image: PropTypes.string.isRequired,
         year: PropTypes.string.isRequired
     }),
-    onClick: PropTypes.func.isRequired
+    onClick: PropTypes.func
 };
