eebf84c6dda7bb40c950deb4c1fcda7256ed3a1d
timmagithub <80230635+timmagithub@users.noreply.github.com>
2021-12-15 21:19:24 -0500

    connected movie card to store

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -4,12 +4,14 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 
 import './movie-card.scss';
 
-export class MovieCard extends React.Component {
+class MovieCard extends React.Component {
     render() {
-        const handleSubmit = (e) => {
+        const handleAddMovie = (e) => {
             const token = localStorage.getItem('token');
             const user = localStorage.getItem('user');
             axios.post(`https://quikflix.herokuapp.com/users/${user}/myMovies/` + 
@@ -17,30 +19,52 @@ export class MovieCard extends React.Component {
                 .then(response => {
                     const data = response.data;
                     console.log(data);
-                    alert('Movie was added to your favorites. (If this was done in error, go to profile page to remove it.');
+                    alert('Movie was added to your favorites.');
+                    location.reload();
+                    return false;
                 })
                 .catch(e=> {
                     console.log('error adding movie to your favorites')
                 });
-          };
+        };
 
-        const { movie } = this.props;
+        const handleDeleteMovie = (e) => {
+            const token = localStorage.getItem('token');
+            const user = localStorage.getItem('user');
+            axios.delete(`https://quikflix.herokuapp.com/users/${user}/myMovies/` + 
+                this.props.movie._id, {}, {headers: { Authorization: `Bearer ${token}` }, })
+                .then(response => {
+                    const data = response.data;
+                    console.log(data);
+                    alert('Movie was deleted from your favorites.');
+                    location.reload();
+                    return false;
+                })
+                .catch(e=> {
+                    console.log('error deleting movie from your favorites')
+                });
+        };
 
+        const { movie, user } = this.props;
+        
         return ( 
             <Container className="card-container">
-                <Card className="movie-card">
+                <Card className="card-display">
                     <Card.Img crossOrigin="anonymous" className="card-img" variant="top" src={movie.image} />
 
                         <Card.Body className="card-body">
                             <Card.Title className="card-title" >{movie.title}</Card.Title>
                         <Card.Text className="card-text" >{movie.year}</Card.Text>
-                        <div>
-                                <Link to={`/movies/${movie.title}`} >
-                                    <Button variant="link">Open</Button>
-                                </Link>
-                                <Button variant="primary" className="card-btn" type="submit" onClick={handleSubmit}>
-                                Add to Favs
-                                </Button>
+                        <div className="card-btns">
+                            <Link to={`/movies/${movie.title}`} >
+                               <Button className="open-btn" variant="primary">Open</Button>
+                            </Link>
+                            {user.movieList.find(m => m === movie._id) ? <Button variant="primary" className="card-btn" type="submit" onClick={handleDeleteMovie}>
+                                Del
+                            </Button> : <Button variant="primary" className="card-btn" type="submit" onClick={handleAddMovie}>
+                                Add
+                            </Button>
+                            }
                         </div>    
                         </Card.Body>
                     </Card>
@@ -49,3 +73,9 @@ export class MovieCard extends React.Component {
     }
 }
 
+let mapStateToProps = state => {
+    return { movies: state.movies,
+    user: state.user}
+}
+
+export default connect(mapStateToProps)(MovieCard);
