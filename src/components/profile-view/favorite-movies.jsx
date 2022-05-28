532f02c8043bf6d23d2297b520c228e5be52574d
timmagithub <80230635+timmagithub@users.noreply.github.com>
2022-05-28 11:07:03 -0400

    removed Col bootstrap

--- src/components/profile-view/favorite-movies.jsx
+++ src/components/profile-view/favorite-movies.jsx
@@ -13,9 +13,11 @@ export function FavoriteMovies({ movieList, movies }) {
             <h2>Favorite Movies</h2>
             <div className="movie-list">
             {movies.filter(f => movieList.find(m => f._id === m)).map(m => 
-            <Col>
-                <MovieCard movie={m} key={m._id} className="fav-card" />
-            </Col>    
+               
+                    <div className="fav-card">
+                        <MovieCard movie={m} key={m._id} />
+                        </div>
+               
         )}
         </div>
         </div>
